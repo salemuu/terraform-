add4a816e2b54915bd51686958c86c4aefaf2fd5
salemuu <39748037+salemuu@users.noreply.github.com>
2020-06-28 01:24:04 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.0.0"
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
